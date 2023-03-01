080b679ab4dadd5c8c64a159789faa7e146e4cd7
Prateek Singla <singla.prateek@gmail.com>
2023-03-01 10:35:58 +0000

    Line spacing

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import gspread, os, json
 from pushbullet import PushBullet
 
-# Proxy Self Signed Certificate Workaround
+#Corporate Proxy Self Signed Certificate Workaround
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
+
 #Pushbullet config
 pb = PushBullet(os.environ['PB_KEY'])
+
 #Google Cloud Key Config
 credentials = json.loads(os.environ['GC_CRED'])
+
 #Access Google Sheets file
 gc = gspread.service_account_from_dict(credentials)
 wks = gc.open("Equity Analysis Template").worksheet("Alerts")
